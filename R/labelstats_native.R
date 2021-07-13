85c9c17c25c103920340ba02422581cf91bfdb2e
Stathis Gennatas <gennatas@gmail.com>
2021-07-12 20:09:12 -0700

    init labelstats_native

---
+++ R/labelstats_native.R
@@ -0,0 +1,83 @@
+# labelstats_native.R
+# ::prprcss::
+# 2021 E.D. Gennatas lambdamd.org
+
+#' Get label stats for a list of images using \pkg{ANTsR}
+#'
+#' @param x Character vector of nifti paths OR list of \code{antsImage} objects in native space
+#' @param labeled_nifti Character of path to labeled nifti OR \code{antsImage} object
+#' @param labelkey Character vector: Label names
+#' @param exclude_label_index Integer vector: 1-based index of labels to exclude. Default = 1,
+#' which would exclude the first label that is commonly the background. Set to NULL to not exclude
+#' any labels
+#' @param verbose Logical: If TRUE, print messages to console. Default = TRUE
+#' 
+#' @author E.D. Gennatas
+#' @export
+
+labelstats_native <- function(x, labeled_nifti,
+                              labelkey = NULL,
+                              exclude_label_index = 1,
+                              do_save_native_label = TRUE,
+                              verbose = TRUE) {
+
+  labeled_nifti <- ANTsRCore::check_ants(labeled_nifti)
+  labeled_nifti_filename <- filename(labeled_nifti@filename)
+  if (is.null(labelkey)) {
+    labelkey <- paste0("Label_", seq_len(length(unique(labeled_nifti))) - 1)
+  }
+  nimgs <- length(x)
+  # init id vector
+  id <- character(nimgs)
+  # init labelstats list
+  .ls <- vector("list", nimgs)
+
+  # labelStats ====
+  for (i in seq_along(x)) {
+    if (verbose) msg0("Working on image ", i, " of ", nimgs, "...")
+    img <- ANTsRCore::check_ants(x[i])
+    name <- gsub("_.*", "", filename(img@filename))
+    id[i] <- filename(img@filename)
+    dirpath <- dirname(x[i])
+    # native label path
+    native_label_path <- file.path(dirpath, paste0(name, "_", labeled_nifti_filename, "_nativeMasked.nii.gz"))
+    if (!file.exists(native_label_path)) {
+      affine <- file.path(dirpath, dir(dirpath, paste0(name, ".*0GenericAffine")))
+      warp <- file.path(dirpath, dir(dirpath, paste0(name, ".*1InverseWarp")))
+        labeled_nifti_native <- ANTsRCore::antsApplyTransforms(
+            fixed = img,
+            moving = labeled_nifti,
+            transformlist = c(affine, warp),
+            interpolator = "genericLabel",
+            verbose = verbose)
+        
+        # mask native label by native GM
+        native_mask <- ANTsRCore::getMask(img, cleanup = 0)
+        labeled_nifti_native_masked <- labeled_nifti_native * native_mask
+        
+        if (do_save_native_label) {
+          ## +++Write native labels to file ====
+          if (verbose) msg("Writing masked native label file to disk...")
+            ANTsRCore::antsImageWrite(
+                image = labeled_nifti_native_masked,
+                filename = native_label_path)
+          if (file.exists(native_label_path)) {
+            
+          }
+        }
+    }
+    .ls[[i]] <- ANTsRCore::labelStats(img, labeled_nifti)
+  }
+  names(.ls) <- id
+
+  dat <- lapply(seq_along(.ls),
+                function(i) data.table(ID = id[i], .ls[[i]]))
+  dat <- do.call(rbind, dat)
+  dat_wide <- dcast(dat, ID ~ LabelValue, value.var = "Mean")
+  names(dat_wide)[2:ncol(dat_wide)] <- labelkey
+  if (!is.null(exclude_label_index)) {
+    dat_wide <- dat_wide[, -c(exclude_label_index + 1)]
+  }
+  dat_wide
+
+} # prprcss::labelstats_native
