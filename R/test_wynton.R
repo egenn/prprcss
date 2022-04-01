f5581e0ed11b7403d9a5d1a9231b678bf2ee7442
Stathis Gennatas <gennatas@gmail.com>
2022-03-31 21:44:55 -0700

    up

--- R/test_wynton.R
+++ R/test_wynton.R
@@ -1,18 +1,23 @@
-# test_sge.R
+# test_wynton.R
 # ::prprcss::
 # 2022 E. D. Gennatas lambdamd.org
 
-#' Test ANTsX works on the SGE cluster
+#' Test our software works on Wynton
+#' 
+#' Tests ANTsX loads
 #' 
 #' @export 
 
-test_sge <- function() {
+test_wynton <- function() {
     sessionInfo()
     library(ANTsRCore)
     library(ANTsR)
     library(ANTsRNet)
 
     sink(file = paste0("~/.prprcss_", format(Sys.time(), "%Y%m%d-%H%M%S"), ".log"))
+    cat("R.home():\n")
+    R.home()
+    cat("sessionInfo():\n")
     sessionInfo()
     sink()
 } # prprcss::test_sge
