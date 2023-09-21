3dbf0b667c7dab3eec4ca60699444e3e36ef8dbe
Stathis Gennatas <gennatas@gmail.com>
2023-09-21 12:51:06 -0700

    cleanup

--- R/labelstats_native.R
+++ R/labelstats_native.R
@@ -16,18 +16,17 @@
 #' i.e. do not exclude any labels. May be best to include a \code{labelkey} that excludes unwanted
 #' labels (e.g. the background)
 #' @param verbose Logical: If TRUE, print messages to console. Default = TRUE
-#' 
+#'
 #' @author E.D. Gennatas
 #' @export
 
 labelstats_native <- function(
-    x, 
+    x,
     labeled_nifti,
     labelkey = NULL,
     exclude_label_index = NULL,
     do_save_native_label = TRUE,
     verbose = TRUE) {
-
   labeled_nifti <- ANTsRCore::check_ants(labeled_nifti)
   labeled_nifti_filename <- filename(labeled_nifti@filename)
   # if (is.null(labelkey)) {
@@ -55,62 +54,68 @@ labelstats_native <- function(
     if (!file.exists(native_label_path)) {
       affine <- file.path(dirpath, dir(dirpath, paste0(name, ".*0GenericAffine")))
       warp <- file.path(dirpath, dir(dirpath, paste0(name, ".*1InverseWarp")))
-        labeled_nifti_native <- ANTsRCore::antsApplyTransforms(
-            fixed = img,
-            moving = labeled_nifti,
-            transformlist = c(affine, warp),
-            interpolator = "genericLabel",
-            verbose = verbose)
-        
-        # mask native label by native GM
-        native_mask <- ANTsRCore::getMask(img, cleanup = 0)
-        labeled_nifti_native_masked <- labeled_nifti_native * native_mask
-        
-        if (do_save_native_label) {
-          ## +++Write native labels to file ----
-          if (verbose) msg("Writing masked native label file to disk...")
-            ANTsRCore::antsImageWrite(
-                image = labeled_nifti_native_masked,
-                filename = native_label_path)
-          if (file.exists(native_label_path)) {
-            msg("Saved", native_label_path)
-          } else {
-            warning("Failed to save", native_label_path)
-          }
+      labeled_nifti_native <- ANTsRCore::antsApplyTransforms(
+        fixed = img,
+        moving = labeled_nifti,
+        transformlist = c(affine, warp),
+        interpolator = "genericLabel",
+        verbose = verbose
+      )
+
+      # mask native label by native GM
+      native_mask <- ANTsRCore::getMask(img, cleanup = 0)
+      labeled_nifti_native_masked <- labeled_nifti_native * native_mask
+
+      if (do_save_native_label) {
+        ## +++Write native labels to file ----
+        if (verbose) msg("Writing masked native label file to disk...")
+        ANTsRCore::antsImageWrite(
+          image = labeled_nifti_native_masked,
+          filename = native_label_path
+        )
+        if (file.exists(native_label_path)) {
+          msg("Saved", native_label_path)
+        } else {
+          warning("Failed to save", native_label_path)
         }
+      }
     } else {
       labeled_nifti_native_masked <- ANTsRCore::check_ants(native_label_path)
     }
     .ls[[i]] <- ANTsRCore::labelStats(img, labeled_nifti_native_masked)
   }
   names(.ls) <- id
-  
+
   if (verbose) msg("Merging labelstats of", nimgs, "volumes...")
   # Some labels may be missing from native space:
   # Fix by merging with all unique Label IDs before rbinding
   dat <- data.table(LabelID = sort(unique(labeled_nifti)))
   if (!is.null(labelkey)) {
-      if (all(c("LabelID", "LabelName") %in% names(labelkey))) {
-          dat <- merge(dat, labelkey, by = "LabelID")
-      }
+    if (all(c("LabelID", "LabelName") %in% names(labelkey))) {
+      dat <- merge(dat, labelkey, by = "LabelID")
+    }
   }
-  datlabels <- lapply(.ls, \(l) 
-      merge(dat, l, by.x = "LabelID", by.y = "LabelValue", all = TRUE))
-  datlabels <- lapply(seq_along(datlabels),
-                \(i) data.table(ImageID = id[i], datlabels[[i]]))
+  datlabels <- lapply(.ls, \(l) {
+    merge(dat, l, by.x = "LabelID", by.y = "LabelValue", all = TRUE)
+  })
+  datlabels <- lapply(
+    seq_along(datlabels),
+    \(i) data.table(ImageID = id[i], datlabels[[i]])
+  )
   datlabels <- do.call(rbind, datlabels)
   if (is.null(labelkey)) {
-      dat_wide <- dcast(datlabels, ImageID ~ LabelID,
-      value.var = c("Mean", "Volume"))
+    dat_wide <- dcast(datlabels, ImageID ~ LabelID,
+      value.var = c("Mean", "Volume")
+    )
   } else {
-      dat_wide <- dcast(datlabels, ImageID ~ LabelID + LabelName,
-      value.var = c("Mean", "Volume"))
+    dat_wide <- dcast(datlabels, ImageID ~ LabelID + LabelName,
+      value.var = c("Mean", "Volume")
+    )
   }
-  
+
   if (!is.null(exclude_label_index)) {
     # 1st column is ImageID, labels start at 2nd column
-    dat_wide <- dat_wide[, - c(exclude_label_index + 1)]
+    dat_wide <- dat_wide[, -c(exclude_label_index + 1)]
   }
   dat_wide
-
 } # prprcss::labelstats_native
--- R/preprocess_t1.R
+++ R/preprocess_t1.R
@@ -37,148 +37,172 @@ preprocess_t1 <- function(x,
                           vistrace = 0,
                           vistrace_slices = c(90, 130, 170),
                           itk_cores = NULL) {
+  if (!is.null(itk_cores)) {
+    Sys.setenv(ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS = itk_cores)
+  }
+  if (is.null(template_brain)) {
+    template_brain <- ANTsRCore::antsImageRead(
+      system.file("data", "antsmni.nii.gz", package = "prprcss")
+    )
+    template_brain_name <- "mni"
+  } else {
+    template_brain <- ANTsRCore::check_ants(template_brain)
+    template_brain_name <- filename(template_brain@filename)
+  }
 
-    if (!is.null(itk_cores)) {
-        Sys.setenv(ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS = itk_cores)
-    }
-    if (is.null(template_brain)) {
-        template_brain <- ANTsRCore::antsImageRead(
-            system.file("data", "antsmni.nii.gz", package = "prprcss"))
-            template_brain_name <- "mni"
+  for (i in seq_along(x)) {
+    ## Read image ----
+    if (verbose) cat("Reading image ", x[i], "...")
+    t1 <- ANTsRCore::check_ants(x[i])
+    .id <- if (is.null(id[i])) filename(t1@filename) else id[i]
+
+    # Output directory
+    if (is.null(outdir)) {
+      outdir <- file.path(dirname(x[i]), "preprocess_t1")
     } else {
-        template_brain <- ANTsRCore::check_ants(template_brain)
-        template_brain_name <- filename(template_brain@filename)
+      outdir <- normalizePath(outdir)
     }
-    
-    for (i in seq_along(x)) {
 
-        ## Read image ----
-        if (verbose) cat("Reading image ", x[i], "...")
-        t1 <- ANTsRCore::check_ants(x[i])
-        .id <- if (is.null(id[i])) filename(t1@filename) else id[i]
+    if (vistrace > 0) {
+      plot(t1, axis = 3, slices = vistrace_slices, title.img = "Raw T1")
+    }
 
-        # Output directory
-        if (is.null(outdir)) {
-            outdir <- file.path(dirname(x[i]), "preprocess_t1")
-        } else {
-            outdir <- normalizePath(outdir)
-        }
+    ## 1. N4 bias correction ----
+    if (verbose) cat("Running N4 bias correction...")
+    t1 <- ANTsR::abpN4(t1,
+      intensityTruncation = intensityTruncation,
+      verbose = verbose
+    )
+    if (vistrace > 0) {
+      plot(t1,
+        axis = 3, slices = vistrace_slices,
+        title.img = "N4-corrected T1", title.line = -3
+      )
+    }
 
-        if (vistrace > 0) {
-            plot(t1, axis = 3, slices = vistrace_slices, title.img = "Raw T1")
-        }
+    ## 2. Brain extraction ----
+    if (verbose) cat("Performing brain extraction...")
+    t1_brain_mask <- ANTsRNet::brainExtraction(t1, modality = "t1")
+    # PR: input arg test warning
+    t1_brain_mask <- ANTsRCore::getMask(t1_brain_mask, lowThresh = .5, highThresh = 1)
+    if (vistrace > 0) {
+      plot(t1_brain_mask,
+        axis = 3, slices = vistrace_slices,
+        title.img = "Brain mask", title.line = -3
+      )
+    }
 
-        ## 1. N4 bias correction ----
-        if (verbose) cat("Running N4 bias correction...")
-        t1 <- ANTsR::abpN4(t1, 
-                intensityTruncation = intensityTruncation,
-                verbose = verbose)
-        if (vistrace > 0) {
-            plot(t1, axis = 3, slices = vistrace_slices,
-                title.img = "N4-corrected T1", title.line = -3)
-        }
+    t1_brain <- t1 * t1_brain_mask
+    if (vistrace > 0) {
+      plot(t1_brain,
+        axis = 3, slices = vistrace_slices,
+        title.img = "T1 brain", title.line = -3
+      )
+    }
 
-        ## 2. Brain extraction ----
-        if (verbose) cat("Performing brain extraction...")
-        t1_brain_mask <- ANTsRNet::brainExtraction(t1, modality = "t1")
-        # PR: input arg test warning
-        t1_brain_mask <- ANTsRCore::getMask(t1_brain_mask, lowThresh = .5, highThresh = 1)
-        if (vistrace > 0) {
-            plot(t1_brain_mask, axis = 3, slices = vistrace_slices,
-                title.img = "Brain mask", title.line = -3)
-        }
-        
-        t1_brain <- t1 * t1_brain_mask
-        if (vistrace > 0) {
-            plot(t1_brain, axis = 3, slices = vistrace_slices,
-                title.img = "T1 brain", title.line = -3)
-        }
+    ## 3. SyN to template brain ----
+    transform_type_name <- if (grepl("antsRegistrationSyN", transform_type)) {
+      "SyN"
+    } else {
+      transform_type
+    }
+    dir.create(outdir, showWarnings = FALSE)
+    if (verbose) cat("Registering to template...")
+    t1_brain_to_template_brain <- ANTsRCore::antsRegistration(
+      fixed = template_brain,
+      moving = t1_brain,
+      typeofTransform = transform_type,
+      outprefix = file.path(outdir, paste0(
+        .id, "_brain_to_",
+        template_brain_name, "_", transform_type_name
+      )),
+      printArgs = verbose,
+      verbose = verbose
+    )
+    # > t1_brain_to_template_brain$invtransforms
+    # [1] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc0GenericAffine.mat"
+    # [2] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc1InverseWarp.nii.gz"
+    # > t1_brain_to_template_brain$fwdtransforms
+    # [1] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc1Warp.nii.gz"
+    # [2] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc0GenericAffine.mat"
 
-        ## 3. SyN to template brain ----
-        transform_type_name <- if (grepl("antsRegistrationSyN", transform_type)) {
-            "SyN"
-        } else {
-            transform_type
-        }
-        dir.create(outdir, showWarnings = FALSE)
-        if (verbose) cat("Registering to template...")
-        t1_brain_to_template_brain <- ANTsRCore::antsRegistration(
-            fixed = template_brain,
-            moving = t1_brain,
-            typeofTransform = transform_type,
-            outprefix = file.path(outdir, paste0(.id, "_brain_to_",
-                template_brain_name, "_", transform_type_name)),
-            printArgs = verbose,
-            verbose = verbose)
-# > t1_brain_to_template_brain$invtransforms
-# [1] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc0GenericAffine.mat" 
-# [2] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc1InverseWarp.nii.gz"
-# > t1_brain_to_template_brain$fwdtransforms
-# [1] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc1Warp.nii.gz"      
-# [2] "/tmp/Rtmp7NkZw9/file2faf3ba99bfc0GenericAffine.mat"
+    ## 3b. Apply transformation ----
+    t1_brain_W <- ANTsRCore::antsApplyTransforms(
+      fixed = template_brain,
+      moving = t1_brain,
+      transformlist = t1_brain_to_template_brain$fwdtransforms,
+      verbose = verbose
+    )
 
-        ## 3b. Apply transformation ----
-        t1_brain_W <- ANTsRCore::antsApplyTransforms(
-            fixed = template_brain,
-            moving = t1_brain,
-            transformlist = t1_brain_to_template_brain$fwdtransforms,
-            verbose = verbose)
+    ## 4. Normalize ----
+    t1_brain_Wn <- ANTsRCore::iMath(
+      img = t1_brain_W,
+      operation = "Normalize"
+    )
 
-        ## 4. Normalize ----
-        t1_brain_Wn <- ANTsRCore::iMath(
-            img = t1_brain_W,
-            operation = "Normalize")
+    if (vistrace > 1) {
+      plot(t1_brain_Wn,
+        axis = 3, slices = vistrace_slices,
+        title.img = "Warped, Normalized T1", title.line = -3
+      )
+    }
 
-        if (vistrace > 1) {
-            plot(t1_brain_Wn, axis = 3, slices = vistrace_slices,
-                title.img = "Warped, Normalized T1", title.line = -3)
-        }
+    ## +++Write rsWn to file ----
+    ANTsRCore::antsImageWrite(
+      image = t1_brain_Wn,
+      filename = file.path(outdir, paste0(.id, "_rsWn.nii.gz"))
+    )
 
-        ## +++Write rsWn to file ----
-        ANTsRCore::antsImageWrite(image = t1_brain_Wn,
-            filename = file.path(outdir, paste0(.id, "_rsWn.nii.gz")))
+    ## 6. Atropos Segmentation ----
+    if (verbose) cat("Performing Atropos segmentation...")
+    t1_3class <- atropos(
+      a = t1_brain,
+      x = t1_brain_mask,
+      i = atropos_initialization,
+      m = atropos_mrf,
+      c = atropos_convergence,
+      verbose = verbose
+    )
 
-        ## 6. Atropos Segmentation ----
-        if (verbose) cat("Performing Atropos segmentation...")
-        t1_3class <- atropos(a = t1_brain,
-                             x = t1_brain_mask,
-                             i = atropos_initialization,
-                             m = atropos_mrf,
-                             c = atropos_convergence,
-                             verbose = verbose)
+    if (vistrace > 0) {
+      plot(t1_3class$probabilityimages[[2]],
+        axis = 3,
+        slices = vistrace_slices,
+        title.img = "Native GM", title.line = -3
+      )
+    }
 
-        if (vistrace > 0) {
-            plot(t1_3class$probabilityimages[[2]], axis = 3,
-                slices = vistrace_slices,
-                title.img = "Native GM", title.line = -3)
-        }
+    ## +++Write native GM to file ----
+    ANTsRCore::antsImageWrite(
+      image = t1_3class$probabilityimages[[2]],
+      filename = file.path(outdir, paste0(.id, "_rsGM.nii.gz"))
+    )
 
-        ## +++Write native GM to file ----
-        ANTsRCore::antsImageWrite(image = t1_3class$probabilityimages[[2]],
-            filename = file.path(outdir, paste0(.id, "_rsGM.nii.gz")))
+    ## Warp GM ----
+    if (do_warpgm) {
+      ## Apply transformation to GM ----
+      gmW <- ANTsRCore::antsApplyTransforms(
+        fixed = template_brain,
+        moving = t1_3class$probabilityimages[[2]],
+        transformlist = t1_brain_to_template_brain$fwdtransforms,
+        verbose = verbose
+      )
 
-        ## Warp GM ----
-        if (do_warpgm) {
-            ## Apply transformation to GM ----
-            gmW <- ANTsRCore::antsApplyTransforms(
-                fixed = template_brain,
-                moving = t1_3class$probabilityimages[[2]],
-                transformlist = t1_brain_to_template_brain$fwdtransforms,
-                verbose = verbose)
+      if (vistrace > 0) {
+        plot(gmW,
+          axis = 3,
+          # slices = vistrace_slices,
+          title.img = "Warped GM", title.line = -3
+        )
+      }
 
-            if (vistrace > 0) {
-                plot(gmW, axis = 3,
-                    # slices = vistrace_slices,
-                    title.img = "Warped GM", title.line = -3)
-            }
-
-            ## +++Write gmW to file ----
-            ANTsRCore::antsImageWrite(image = gmW,
-                filename = file.path(outdir, paste0(.id, "_rsGMW.nii.gz")))
-        }
-
-    } # /loop through input images
-
+      ## +++Write gmW to file ----
+      ANTsRCore::antsImageWrite(
+        image = gmW,
+        filename = file.path(outdir, paste0(.id, "_rsGMW.nii.gz"))
+      )
+    }
+  } # /loop through input images
 } # prprcss::preprocess_t1
 
 filename <- function(x) gsub("\\..*", "", basename(x))
